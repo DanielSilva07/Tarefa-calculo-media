301f7be261dd673eb549480a0d5c4416fd322c98
danielsilva07 <daniel97silva.ds@gmail.com>
2024-02-19 16:32:20 -0300

    Tarefa-calculo-media

---
+++ src/CalculoNotas.java
@@ -0,0 +1,28 @@
+public class CalculoNotas {
+
+    public static void main (String Args []) {
+
+
+
+        int mediaTotal1 = 0;
+        int mediaTotal2 = 0;
+        int mediaTotal3 = 0;
+        int mediaTotal4 = 0;
+
+         int nota1 = 60;
+         int nota2 = 25;
+         int nota3 = 30;
+         int nota4 = 70;
+
+        mediaTotal1 = nota1 / 4 ;
+        mediaTotal2 = nota2 / 4 ;
+        mediaTotal3 = nota3 / 4 ;
+        mediaTotal4 = nota4 / 4 ;
+
+        System.out.println(mediaTotal1);
+        System.out.println(mediaTotal2);
+        System.out.println(mediaTotal3);
+        System.out.println(mediaTotal4);
+
+    }
+}
